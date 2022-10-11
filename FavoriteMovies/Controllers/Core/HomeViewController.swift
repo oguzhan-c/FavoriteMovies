c9c901f344b0941d4084f4c6940a594e6b0a66ec
Oğuzhan Can <oguzhan.can.iletisim@gmail.com>
2022-10-11 12:51:10 +0300

    Project created and added base views

---
+++ FavoriteMovies/Controllers/Core/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  FavoriteMovies
+//
+//  Created by Oğuzhan Can on 11.10.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        view.backgroundColor = .systemRed
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FavoriteMovies/Controllers/Core/MainTabBarViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MainTabBarViewController.swift
+//  FavoriteMovies
+//
+//  Created by Oğuzhan Can on 11.10.2022.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        
+        configureViews()
+    }
+    
+    private func configureViews(){
+        let vcHome = UINavigationController(rootViewController: HomeViewController())
+        let vcUpcoming = UINavigationController(rootViewController: UpcomingViewController())
+        let vcSearch = UINavigationController(rootViewController: SearchViewController())
+        let vcWatchList = UINavigationController(rootViewController: WatchListViewController())
+       
+        vcHome.tabBarItem.image = UIImage(systemName: "house.circle")
+        vcUpcoming.tabBarItem.image = UIImage(systemName: "play.circle")
+        vcSearch.tabBarItem.image = UIImage(systemName: "magnifyingglass.circle")
+        vcWatchList.tabBarItem.image = UIImage(systemName: " hand.thumbsup.circle.fill")
+        setViewControllers([
+            vcHome ,
+            vcUpcoming ,
+            vcSearch ,
+            vcWatchList
+        ], animated: true)
+        
+        vcHome.title = "Home"
+        vcSearch.title = "Search"
+        vcUpcoming.title = "Coming Soon"
+        vcWatchList.title = "Favorite"
+        
+        tabBar.tintColor = .label
+    }
+
+}
